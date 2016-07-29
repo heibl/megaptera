09e063b9cc89c1549dffab121b3f2756c330fcf2
heibl <heibsta@gmx.net>
2016-07-29 21:50:40 +0200

    findRoot

--- R/findRoot.R
+++ R/findRoot.R
@@ -6,5 +6,11 @@ findRoot <- function(tax){
   id <- apply(tax, 2, function(y) length(unique(y)))
   id <- cbind(cumsum(id), cumsum(rep(1, length(id))))
   id <- max(which(id[, 1] == id[, 2]))
-  tax[1, id]
+  r <- tax[1, id]
+  if ( r == "-" )
+    while ( r == "-" ) {
+      id <- id - 1
+      r <- tax[1, id]
+    }
+  r
 }
