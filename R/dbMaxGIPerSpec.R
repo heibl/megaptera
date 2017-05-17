6bdd46a6902a3606845bff0451a7e808120d0a84
Christoph Heibl <heibsta@gmx.net>
2017-05-17 11:07:00 +0200

    bug fix in dbMaxGISpec [Issue: Latrodectus herperus]

--- R/dbMaxGIPerSpec.R
+++ R/dbMaxGIPerSpec.R
@@ -1,5 +1,5 @@
 ## This code is part of the megaptera package
-## © C. Heibl 2014 (last update 2017-02-20)
+## © C. Heibl 2014 (last update 2017-05-17)
 
 #' @export
 #' @import DBI
@@ -12,23 +12,22 @@ dbMaxGIPerSpec <- function(x, max.gi.per.spec){
   acc.tab <- paste("acc", gsub("^_", "", gene), sep = "_")
   logfile <- paste0("log/", gene, "-stepB.log")
   
-  if ( missing(max.gi.per.spec) ) 
+  if (missing(max.gi.per.spec)) 
     max.gi.per.spec <- x@params@max.gi.per.spec
   
   conn <- dbconnect(x@db)
   taxon <- paste("SELECT gi, taxon, npos", 
                  "FROM", acc.tab,
                  "WHERE status != 'excluded (indet)'",
-                 "AND status != 'excluded (too long)'"
-                 )
+                 "AND status != 'excluded (too long)'")
   taxon <- dbGetQuery(conn, taxon)
   freqs <- table(taxon$taxon)
-  if ( any(id <- freqs > max.gi.per.spec) ){
+  if (any(id <- freqs > max.gi.per.spec)){
     id <- names(id)[id]
     slog("\n..", length(id), "species have >", 
          max.gi.per.spec, "sequenes:", 
          file = logfile)
-    for ( i in id ){
+    for (i in id){
       acc <- taxon[taxon$taxon == i, ]
       acc <- acc[order(acc$npos, decreasing = TRUE), ]
       acc.exclude <- tail(acc$gi, -max.gi.per.spec)
@@ -42,7 +41,7 @@ dbMaxGIPerSpec <- function(x, max.gi.per.spec){
       acc <- paste("UPDATE", acc.tab, 
                    "SET status='raw'",
                    "WHERE", wrapSQL(acc.include, "gi", "=", "OR"))
-      dbSendQuery(conn, acc)
+      lapply(acc, dbSendQuery, conn = conn)
     }
   }
   dbDisconnect(conn)
